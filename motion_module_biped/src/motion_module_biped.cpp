e632ccc68ca52d936b5c523d9f1babff395b96d4
Shuuji Kajita <shuuji.kajita@gmail.com>
2024-03-02 10:28:24 +0900

    [fixed] correct joint_dir, pose_offset

--- motion_module_biped/src/motion_module_biped.cpp
+++ motion_module_biped/src/motion_module_biped.cpp
@@ -56,9 +56,9 @@ MotionModuleBiped::MotionModuleBiped()
 	pose_offset= Eigen::VectorXd::Zero(JointNameList.size());
 	joint_dir  = Eigen::VectorXd::Zero(JointNameList.size());
 	
-	joint_dir << 1,1,1,1,1,1,  1,1,-1,-1,-1,1;
+	joint_dir << 1.9048,-1,1,1,1,1,  1.9048,-1,-1,-1,-1,1;		// align joint rotation direction to XYZ frame (hip_yaw= 40T/21T)
 
-	pose_offset << 0.15,0,0,0,0,0,   0,0,0,0,0,0;
+	pose_offset << 0.0787,0,0,-0.85,0,0,   0,0,0,-0.85,0,0;
 
 	start_time = 0.0;
   T_interp   = 1.0;
@@ -68,10 +68,11 @@ MotionModuleBiped::MotionModuleBiped()
 	PoseNameList.clear(); PoseList.clear();
 	Eigen::VectorXd pose(JointNameList.size()); 
 	
-	PoseNameList.push_back("initial");     pose << 0, 0,    0, -0.85,    0, 0,  0.0, 0,   0, -0.85,   0, 0; PoseList.push_back(pose);
-	PoseNameList.push_back("halfsitting"); pose << 0, 0, -0.5,  0.15, -0.5, 0,  0.0, 0, -0.5,  0.15, -0.5, 0; PoseList.push_back(pose);  
-	PoseNameList.push_back("squat");       pose << 0, 0, -1.0,  1.15, -1.0, 0,  0.0, 0, -1.0,  1.15, -1.0, 0; PoseList.push_back(pose);  
-	PoseNameList.push_back("openhipyaw");  pose << -3.14,0,    0, -0.85,    0, 0,  3.14,0,   0,  -0.85,   0, 0; PoseList.push_back(pose);  
+	PoseNameList.push_back("initial");     pose << 0,0,0,0,0,0,        0,0,0,0,0,0;       PoseList.push_back(DEGREE2RADIAN*pose);
+	PoseNameList.push_back("halfsitting"); pose << 0,0,-30,60,-30,0,   0,0,-30,60,-30,0;  PoseList.push_back(DEGREE2RADIAN*pose);  
+	PoseNameList.push_back("squat");       pose << 0,0,-60,120,-60,0,  0,0,-60,120,-60,0; PoseList.push_back(DEGREE2RADIAN*pose);  
+	PoseNameList.push_back("openhipyaw");  pose << -90,0,0,0,0,0,      90,0,0,0,0,0;      PoseList.push_back(DEGREE2RADIAN*pose); 
+	PoseNameList.push_back("spreadlegs");  pose << 0,-20,0,0,0,20,       0,20,0,0,0,-20;       PoseList.push_back(DEGREE2RADIAN*pose); 
 }
 
 MotionModuleBiped::~MotionModuleBiped()
@@ -187,7 +188,7 @@ void MotionModuleBiped::process(std::map<std::string, robotis_framework::Dynamix
       else
         continue;
 
-      start_pose(j) = joint_dir(j)*dxl->dxl_state_->present_position_ - pose_offset(j);		// Dynamixel's position = joint_dir*(start_pose + pose_offset)
+      start_pose(j) = dxl->dxl_state_->present_position_/joint_dir(j) - pose_offset(j);		// Dynamixel's position = joint_dir*(start_pose + pose_offset)
 		}
 		
     std::cout << "start_pose = \n" << start_pose.transpose()*RADIAN2DEGREE << std::endl;
